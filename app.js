88634e17515af809deb52909a1fc496c45cc5d65
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-03-05 11:40:51 -0500

    updated chat with emojis, color, commands, and updates

--- app.js
+++ app.js
@@ -19,21 +19,57 @@ function chatPage(req, res){
 	res.sendFile(__dirname + webPages + "chat.html");
 }
 //routes
-app.get("/", index);;
+app.get("/", index);
 app.get("/chat", chatPage);
 //socket
 // connection and chat receiving.
+var users = {};
+var userCount = 0;
+
+//chat server connection
 io.on("connection", function(socket){
-	console.log("User connected");
+	socket.on("join", function(name){
+		users[socket.id] = name;
+		io.emit("update", users[socket.id] + " has connected to the server!");
+		userCount = Object.keys(users).length;
+		console.log(name + " connected");
+		console.log("Users: " + userCount);
+	});
 	//on chat msg
 	socket.on("chat message", function(msg){
-		io.emit("chat message", msg);
+		//check if list command
+		if(msg.match(/^([\/]list)/i)){
+			var cmdUserList = "";
+			index = 1;
+			for(var vals in users){
+				if(index === userCount){
+					cmdUserList += users[vals] + ".";}
+					else{
+					cmdUserList += users[vals] + ", ";}
+				index++;
+			}
+			io.emit("command", "Users: " + cmdUserList);
+		} else //check if users commands
+		if(msg.match(/^([\/]users)/i)){
+			if(userCount > 1){
+				io.emit("command", "There are " + userCount + " concurrent users.");
+			}
+				else{
+				io.emit("command", "There is " + userCount + " concurrent user.");
+			}
+		} else //default chat message
+		{
+			io.emit("chat message", "" + users[socket.id], msg);
+			}
 		//socket.broadcast.emit("chat message", msg);
-		console.log("User - " + msg);
+		console.log(users[socket.id] + " - " + msg);
 	});
 	//on user disconncet
 	socket.on("disconnect", function(){
-		console.log("User Disconnected");
+		io.emit("update", users[socket.id] + " has disconnected from the server.");
+		console.log(users[socket.id] + " Disconnected.");
+		delete users[socket.id];
+		userCount = Object.keys(users).length;
 	});
 });
 //emit chat messages
--- public/js.min/chat-handler.js
+++ public/js.min/chat-handler.js
@@ -1,26 +1,64 @@
-var userName = prompt("What's your name?");
+var userName = prompt("What's your name? Must be at least 3 characters.");
+var userGiven;
 var dateData = new Date();
 var hours = dateData.getHours();
 var minutes = dateData.getMinutes();
+var timePeriod;
 var socket = io();
-
-function minutePadding(n){
-	if (n.length === 1){n = "0" + n;}
-	return n;
+//setup local time
+function setTimePeriod(){
+	if(hours >= 0 && hours < 13 )
+		{timePeriod = "am";}
+	else
+		{timePeriod = "pm";}
 }
 function normalizeHours(n){
 	if (n > 12){n -= 12;}
 	if (n === 0){n = 12;}
 	return n;
 }
-date = normalizeHours(hours) + ":" + minutePadding(minutes);
-
+function normalizeMinutes(n){
+	if (n < 10){n = "0" + n;}
+	return n;
+}
+date = normalizeHours(hours) + ":" + normalizeMinutes(minutes);
+//socket emit events
+if(userName !== "" && userName.length >= 3){
+	socket.emit("join", userName);
+	userGiven = true;
+}
 $("form").submit(function(){
-	socket.emit("chat message", "[" + date + "] " + userName + ": " + $("#msg").val());
-	$("#msg").val("");
-	return false;
+	if(userGiven === true){
+		setTimePeriod();
+		socket.emit("chat message", $("#msg").val());
+		$("#msg").val("");
+		return false;
+	}
 });
-socket.on("chat message", function(msg){
-	$("#messages").append($("<li>").text(msg) );
+socket.on("chat message", function(who, msg){
+	setTimePeriod();
+	$("#messages").append($("<li class='chat'>").html("[" + date + " " + timePeriod + "] <span class='user'> " + who + "</span>: " + regexFilter(msg) ) );
+	$("#messages")[0].scrollTop = $("#messages")[0].scrollHeight;
+	
+});
+socket.on("update", function(msg){
+	setTimePeriod();
+	$("#messages").append($("<li class='update'>").html("[" + date + " " + timePeriod + "] " + msg) );
 	$("#messages")[0].scrollTop = $("#messages")[0].scrollHeight;
 });
+socket.on("command", function(msg){
+	setTimePeriod();
+	$("#messages").append($("<li class='command'>").html("[" + date + " " + timePeriod + "] " + msg) );
+	$("#messages")[0].scrollTop = $("#messages")[0].scrollHeight;
+});
+// regex for emojis
+function regexFilter(filter){
+	//smiles
+filter = filter.replace(/(:\))/ig, "<img id='smile' src='/images/emojis/smile.png'>");
+filter = filter.replace(/(:\-\))/ig, "<img id='smile' src='/images/emojis/smile.png'>");
+	//indifferents
+filter = filter.replace(/(:\/)/ig, "<img id='indif' src='/images/emojis/indif.png'>");
+filter = filter.replace(/(:\-\/)/ig, "<img id='indif' src='/images/emojis/indif.png'>");
+return filter;
+console.log(filter);
+}
